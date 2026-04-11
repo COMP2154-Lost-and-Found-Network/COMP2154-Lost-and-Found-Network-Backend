14c776b8cd48f30d9bb39763d7b12f741ba7329e
blossom-babs <saphashb@gmail.com>
2026-04-10 20:08:08 -0400

    bug fix: reorder routes

--- routes/items.js
+++ routes/items.js
@@ -4,11 +4,11 @@ import { authenticateToken } from "../middleware/auth.js";
 
 const router = Router();
 
-// protected route
+// protected routes
+router.get("/", authenticateToken, getItems);
 router.post("/", authenticateToken, createItem);
 router.get("/:id", authenticateToken, getItem);
 router.put("/:id", authenticateToken, updateItem);
 router.delete("/:id", authenticateToken, deleteItem);
-router.get("/", authenticateToken, getItems);
 
 export default router;
